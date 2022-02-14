7f72a4aa7ae5e9ba123d347830c8a704a503c37f
Cancel <alexandru.cancel@capgemini.com>
2022-02-14 17:35:19 +0200

    trying to resolve different bugs

--- uni-app/src/app/app.module.ts
+++ uni-app/src/app/app.module.ts
@@ -4,12 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarModule } from 'ng-sidebar';
-import { MobilePhoneComponent } from './mobile-phone/mobile-phone.component';
+import { MobilePhoneComponent, TableDynamicArrayDataExample } from './mobile-phone/mobile-phone.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {CdkTableModule} from '@angular/cdk/table';
+import {MatTableModule} from '@angular/material/table';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialExampleModule} from '../material.module';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
 
 
 
@@ -28,6 +37,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
+    CdkTableModule,
+    MatTableModule,
+
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    TableDynamicArrayDataExample,
+    MatNativeDateModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
